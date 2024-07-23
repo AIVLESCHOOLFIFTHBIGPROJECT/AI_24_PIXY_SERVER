c89ff114b28b39c8bea96a20abb40193c37cd465
Sungjun <bousbaert@gmail.com>
2024-07-23 10:20:01 +0900

    [FIX] 로그인된 사용자의 qna

--- post/views.py
+++ post/views.py
@@ -162,7 +162,7 @@ from drf_yasg import openapi
     responses={201: QnaSerializer, 400: 'Bad Request'}
 )
 @api_view(['GET', 'POST'])
-@permission_classes([AllowAny]) #IsAuthenticated로 바꾸면됨
+@permission_classes([AllowAny]) 
 def QnaList(request):
     # Read
     if request.method == 'GET':
